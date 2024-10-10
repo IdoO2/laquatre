575982503488be0fd62dffbd47186825b972ce7a
Daniel Paterson <dafpaterson@gmail.com>
2024-10-10 10:59:14 +0200

    Add lint tooling

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,20 @@
 import js from '@eslint/js'
+import { includeIgnoreFile } from "@eslint/compat"
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+const fromGitignore = includeIgnoreFile(gitignorePath).ignores || [];
+
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', ...fromGitignore] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
